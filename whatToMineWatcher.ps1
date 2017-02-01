f200ffe1f306a8a520bd4ebc9728829c22f293b8
jrwhite17 <johnrileywhite@gmail.com>
2017-01-31 21:31:11 -0600

    Update 1
    Discovered parameters for whattomine.com calculations, and determined
    calculations for revenue/profit

--- whatToMineWatcher.ps1
+++ whatToMineWatcher.ps1
@@ -2,33 +2,47 @@
 $DEBUG = $true
 $URL = "http://whattomine.com/coins.json"
 
-$RUNNING = $true
+$RUNNING = $true #For "Game" Loop
+
+#Need to determine where BTC Price is pulled from
+$BTC_PRICE = "950"
+
 
 $CoinNameArray = New-Object System.Collections.ArrayList
 $CoinTagArray = New-Object System.Collections.ArrayList
 $CoinAlgorithmArray = New-Object System.Collections.ArrayList
-$CoinProfitabilityArray = New-Object System.Collections.ArrayList
+#revenue = btc_revenue x btc_price
+$CoinRevenueArray = New-Object System.Collections.ArrayList
+#profit = revenue - power cost
+$CoinProfitArray = New-Object System.Collections.ArrayList
 
 
 
-#Invoke-WebRequest $URL |
-#ConvertFrom-JSON |
-#select -expand coins |
-#select -expand . |
-#Select tag, algorithm, profitability
+
+
 
 $JSON = Invoke-WebRequest $URL | ConvertFrom-JSON
 foreach($COIN in $JSON.Coins.psobject.properties.name){
-$CoinNameArray.Add($COIN)
-$CoinTagArray.Add($JSON.Coins.$COIN.tag)
-$CoinAlgorithmArray.Add($JSON.Coins.$COIN.algorithm)
-$CoinProfitabilityArray.Add($JSON.Coins.$COIN.profitability)
+	
+
+	#$CoinNameArray.Add($COIN)
+	#$CoinTagArray.Add($JSON.Coins.$COIN.tag)
+	#$CoinAlgorithmArray.Add($JSON.Coins.$COIN.algorithm)
+	
+	$PROFIT = [Double]$BTC_PRICE * [Double]$JSON.Coins.$COIN.btc_revenue
+	write-output $COIN" -- Revenue:"$PROFIT 
+	
+	#Index
+	#write-output $$JSON.Coins.psobject.properties.name.IndexOf($COIN)
+	
+	
+	
 }
 
-write-output $CoinNameArray
-write-output $CoinTagArray
-write-output $CoinAlgorithmArray
-write-output $CoinProfitabilityArray
+#write-output $CoinNameArray
+#write-output $CoinTagArray
+#write-output $CoinAlgorithmArray
+#write-output $CoinProfitabilityArray
 
 
 
